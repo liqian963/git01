22881c648fb6b154d126c8669eb2a5aea4a8f201
liqian963 <1811095709@qq.com>
2019-11-26 17:05:52 +0800

    idea提交说明

---
+++ src/main/java/com/zking/App.java
@@ -0,0 +1,12 @@
+package com.zking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
