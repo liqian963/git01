338a13dd8e61e5e6745917e4e3583dcdd9efd31b
liqian963 <1811095709@qq.com>
2019-11-26 18:33:50 +0800

    来自于idea工作区间的提交

--- src/main/java/com/zking/utils/CommonUtils.java
+++ src/main/java/com/zking/utils/CommonUtils.java
@@ -3,7 +3,7 @@ package com.zking.utils;
 public class CommonUtils {
 
   public static void main( String[] args ){
-
+      System.out.println("这是来自于idea项目里面的");
       System.out.println( "Hello World!" );
     }
 }
